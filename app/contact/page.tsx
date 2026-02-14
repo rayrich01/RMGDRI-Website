a711f1542acfa1be0a03d73b9996c6d9f5e731ba
Ray Richardson <ray@glorifyjesus.org>
2026-02-13 18:04:44 -0700

    feat: public intake forms + submit endpoint (zod, honeypot, rate limit)

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import IntakeForm from "@/components/intake/IntakeForm";
+
+export default function ContactPage() {
+  return (
+    <IntakeForm
+      type="contact"
+      title="Contact Us"
+      description="Have a question or want to get in touch? Send us a message!"
+    />
+  );
+}
